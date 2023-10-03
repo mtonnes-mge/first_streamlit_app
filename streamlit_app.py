95b1283710bd1992019ba49d9747048495678a3f
mtonnes-mge <146869838+mtonnes-mge@users.noreply.github.com>
2023-10-03 19:22:06 +0200

    Add: default selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 # pick fruits
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick your poisons", list(my_fruit_list.index))
+streamlit.multiselect("Pick your poisons", list(my_fruit_list.index), ['Banana', 'Kiwifruit'])
 
 # display list
 streamlit.dataframe(my_fruit_list)
