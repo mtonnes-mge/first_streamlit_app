d64587b2ab28bf61ab133918950675d4304732a1
mtonnes-mge <146869838+mtonnes-mge@users.noreply.github.com>
2023-10-03 19:12:46 +0200

    Add: menu, contact

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Menu')
+streamlit.text('foo')
+streamlit.text('bar')
+streamlit.text('baz')
+
+streamlit.header('Contact')
+streamlit.text('ping')
