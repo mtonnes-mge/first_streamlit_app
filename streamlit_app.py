5f946f411916244cb0ac0dcb9b22a9861d6ebe49
mtonnes-mge <146869838+mtonnes-mge@users.noreply.github.com>
2023-10-13 17:05:56 +0200

    Chg: conditionally show fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,28 +41,17 @@ try:
 except URLError as e:
     streamlit.error()
 
-streamlit.stop()
-
 ### Snowflake FRUIT LIST ###
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
 
-streamlit.text("Hellow from snowflake:")
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text(my_data_row)
+if streamlit.button("Get fruit load list"):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  streamlit.dataframe(get_fruit_load_list())
 
-streamlit.text("Snowflake context:")
-my_cur.execute("select current_database(), current_schema()")
-my_data_row = my_cur.fetchone()
-streamlit.text(my_data_row)
-
-streamlit.text("The fruit list:")
-my_cur.execute("select * from FRUIT_LOAD_LIST")
-# my_data_row = my_cur.fetchone()
-# streamlit.text(my_data_row)
-my_data_rows = my_cur.fetchall()
-streamlit.dataframe(my_data_rows)
+streamlit.stop()
 
 new_fruit = streamlit.text_input('Add another fruit:')
 my_cur.execute("INSERT INTO FRUIT_LOAD_LIST (FRUIT_NAME) VALUES ('"+new_fruit+"')") # great sql injection
