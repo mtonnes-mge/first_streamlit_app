99ba23aa74385edb9a88cfa7f300de28d98454f4
mtonnes-mge <146869838+mtonnes-mge@users.noreply.github.com>
2023-10-13 16:52:16 +0200

    Stop before snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
+
+### Snowflake FRUIT LIST ###
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
