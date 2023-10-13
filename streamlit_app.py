5d927780d6e25203cfbaa4b7c9384aa172a12b39
mtonnes-mge <146869838+mtonnes-mge@users.noreply.github.com>
2023-10-13 16:05:55 +0200

    Add: show snowflake context

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,9 +36,15 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
+
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hellow from snowflake:")
+streamlit.text(my_data_row)
+
+my_cur.execute("select current_database(), current_schema()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Snowflake context:")
 streamlit.text(my_data_row)
 
 my_cur.execute("select * from fruit_load_list")
