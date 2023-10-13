7484b2c74f79949389e99e6e457976588ed4afb9
mtonnes-mge <146869838+mtonnes-mge@users.noreply.github.com>
2023-10-13 17:14:10 +0200

    Chg: refactor get_cnx, close cnx, add title

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,14 +42,20 @@ except URLError as e:
     streamlit.error()
 
 ### Snowflake FRUIT LIST ###
+streamlit.title("View Our Fruit List - Add Your Favorites!")
+
+def get_cnx():
+  return snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
 
 if streamlit.button("Get fruit load list"):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_cnx = get_cnx()
   streamlit.dataframe(get_fruit_load_list())
+  my_cnx.close()
 
 # input fruit
 def insert_fruit(fruit):
@@ -58,6 +64,7 @@ def insert_fruit(fruit):
 
 fruit_to_add = streamlit.text_input('Add another fruit:')
 if streamlit.button("Add fruit!"):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_cnx = get_cnx()
   insert_fruit(fruit_to_add)
+  my_cnx.close()
   streamlit.write('You added ', fruit_to_add)
